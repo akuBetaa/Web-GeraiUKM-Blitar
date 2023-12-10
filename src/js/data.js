7e17e5b5a559ba97736516a005f124d08c19367c
Beta Nurul Awwalin <beta.n.awwalin@gmail.com>
2023-12-10 09:27:28 +0700

    updated the product page and added the database in src/js/data.js

---
+++ src/js/data.js
@@ -0,0 +1,24 @@
+var products = [ 
+    {
+        id : 1,
+        img : "/src/img/product/product-1.png",
+        name : "Kripik Pare Pak Jo",
+        deskripsi : "Berat 120gram",
+        harga : "12K",
+    },
+    {
+        id : 2,
+        img : "/src/img/product/product-2.png",
+        name : "Sambel Pecel Spesial",
+        deskripsi : "Berat 120gram",
+        harga : "17K",
+    },
+    {
+        id : 1,
+        img : "/src/img/product/product-1.png",
+        name : "Kripik Pare Pak Jo",
+        deskripsi : "Berat 120gram",
+        harga : "12K",
+    },
+]
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,47 @@
 // Menggunakan JavaScript untuk memuat dan menampilkan navbar
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/pages/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        });
+  fetch("/pages/navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+    });
 });
+
+///untuk memanggil list Product
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const productContainer = document.getElementById("product-container");
+
+    // Loop through the products array
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+
+        // Create a new div element for each product
+        const productDiv = document.createElement("div");
+        productDiv.className = "col";
+
+        // Populate the div with product information
+        productDiv.innerHTML = `
+                <div class="item-product shadow p-3 border rounded">
+                <img src=${product.img} class="object-fit-cover border rounded" alt="">
+
+                <div class="row mt-3">
+                    <div class="col-9">
+                        <h5 class="fw-semibold">${product.name}</h5>
+                        <p class="fw-semibold fs-6">${product.deskripsi}</p>
+                    </div>
+                    <div class="col-2">
+                        <h5 class="fw-semibold">${product.harga}</h5>
+                        <button class="btn btn-dark m-0 p-0">
+                            <img src="../src/img/icon-cart.png" class="" alt="">
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // Append the product div to the container
+        productContainer.appendChild(productDiv);
+    }
+});
+
